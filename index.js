4a7cf31b79611edc5dcae9f09074c8681efe77bf
Kayra <kayrabtuncer@gmail.com>
2021-01-09 23:33:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const Telegraf = require('telegraf')
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
