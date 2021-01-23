51f598e2042b9c74d2fb0eb7e05053d39f1354d5
Kayra <kayrabtuncer@gmail.com>
2021-01-23 18:32:05 +0300

    added yarn.lock and edit index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const Telegraf = require('telegraf')
-require('dotenv').config()
+const { Telegraf } = require('telegraf')
+const rateLimit = require('telegraf-ratelimit')
+const monosay = require('monosay').usetelegraf(process.env.MONOSAY_TOKEN)
+
+const limitConfig = {
+  window: 3000,
+  limit: 1,
+  onLimitExceeded: (ctx, next) => ctx.reply('Yeter botu çürüttün')
+}
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+monosay.init(bot)
+bot.use(rateLimit(limitConfig))
+bot.start((ctx) => {
+    console.log('started:', ctx.from.id)
+    monosay.user({
+        channelUserId: ctx.from.id,
+        name: ctx.from.first_name,
+        surname: ctx.from.last_name,
+        userName: ctx.from.username
+    }, /*success callback*/ null, /*error callback*/ null)
+    return ctx.reply('Welcome!')
+})
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('text', (ctx) => ctx.reply('Hello!'))
+bot.hears('hi', (ctx) => {
+    // if(ctx.from.id !== 1223629707) 
+    //     return ctx.reply('I don\'t now you.')
+    return ctx.reply('Hey there')
+})
 bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
